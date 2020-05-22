cdfd7cad0fe80a4625123215d625214a1f1db3bf
yiguozhen0510 <18612842174@163.com>
2020-05-22 17:42:19 +0800

    添加ios vpn登录逻辑

--- ios/Classes/SwiftFlutterVpnPlugin.swift
+++ ios/Classes/SwiftFlutterVpnPlugin.swift
@@ -6,6 +6,18 @@ public class SwiftFlutterVpnPlugin: NSObject, FlutterPlugin {
     let channel = FlutterMethodChannel(name: "flutter_vpn", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterVpnPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+
+    /**
+         一般场景SDK的模式为SFSDKModeSupporVpnSandbox，即同时支持VPN和沙箱功能。
+         需要设置主应用flags，如果需要外部设置沙箱策略需要设置SFSDKFlagsSupportManagePolicy。
+         多应用场景，即一个主应用，多个子应用，所有应用需要开启AppGroup功能且保证groupid一致，同时设置groupid包括sfshare后缀，例如group.com.***.sfshare
+         */
+        [[SFMobileSecuritySDK sharedInstance] initSDK:SFSDKModeSupportVpn
+                                                flags:SFSDKFlagsHostApplication|SFSDKFlagsSupportManagePolicy
+                                                extra:nil];
+
+        [[SFMobileSecuritySDK sharedInstance] setAuthResultDelegate:self];
+
   }
 
 
@@ -43,12 +55,49 @@ public class SwiftFlutterVpnPlugin: NSObject, FlutterPlugin {
 
 
         private func  startPrimaryAuth(call: FlutterMethodCall, result: @escaping FlutterResult){
-
-
+         NSString * vpn_server_url = call.arguments[@"vpn_path_key"];
+         NSString * token_str = call.arguments[@"token_key"];
+         [[SFMobileSecuritySDK sharedInstance] startPasswordAuth:vpn_server_url userName:token_str password:@""];
         }
 
         private func vpnLogout(call: FlutterMethodCall, result: @escaping FlutterResult){
+        [[SFMobileSecuritySDK sharedInstance] cancelAuth];
+        }
 
+        /**
+         认证成功
+         */
+        - (void)onAuthSuccess:(BaseMessage *)msg
+        {
+             NSLog(@"AuthViewController onLoginSuccess");
+            _loginBtn.hidden = FALSE;
+            _cancelLoginBtn.hidden = YES;
 
+            [MBProgressHUD hideHUDForView:self.loginView animated:YES];
+
+            [self showAlertView:@"" message:@"认证成功"];
+            [self showTestList];
+        }
+
+        #pragma mark - SFAuthResultDelegate
+        /**
+         认证失败
+
+         @param msg 错误信息
+         */
+        - (void)onAuthFailed:(BaseMessage *)msg
+        {
+            NSLog(@"AuthViewController onLoginFailed:%@", msg);
+            _loginBtn.hidden = FALSE;
+            _cancelLoginBtn.hidden = YES;
+
+            [MBProgressHUD hideHUDForView:self.loginView animated:YES];
+
+            if(msg.errCode == SF_ERROR_CONNECT_VPN_FAILED && _authType == SFAuthTypeTicket) {
+                [self showTicketAlertView:@"认证失败" message:msg.errStr];
+
+            } else {
+                [self showAlertView:@"认证失败" message:[NSString stringWithFormat:@"%@,code=%ld",msg.errStr,(long)msg.errCode]];
+            }
         }
 }
